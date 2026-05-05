8e3689498926d8104e39fb8ac491e516d995a69b
AlissaGabriel <alissagabriel14@gmail.com>
2026-05-05 17:14:55 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppTempoAgora/Services/DataService.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json.Linq;
+using MauiAppTempoAgora.Models;
+
+namespace MauiAppTempoAgora.Services
+{
+    public class DataService
+    {
+        public static async Task<Tempo?> GetTempo(string cidade)
+        {
+            Tempo? t = null;
+
+            string chave = "53fb370ac763ee7ac16d575813fd7064";
+            string url = $"https://api.openweathermap.org/data/2.5/weather?q={cidade}&units=metric&appid={chave}";
+
+            using(HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage resp = await client.GetAsync(url);
+
+                if (resp.IsSuccessStatusCode)
+                {
+                    string json = await resp.Content.ReadAsStringAsync();
+
+                    var rascunho = JObject.Parse(json);
+
+                    DateTime time = new();
+                    DateTime sunrise = time.AddSeconds((double)rascunho["sys"]["sunrise"]);
+                    DateTime sunset = time.AddSeconds((double)rascunho["sys"]["sunset"]);
+
+                    t = new()
+                    {
+                        lat = (double)rascunho["coord"]["lat"], 
+                        lon = (double)rascunho["coord"]["lon"],
+                        description = (string)rascunho["weather"][0]["main"],
+                        temp_max = (double)rascunho["main"]["temp_max"],
+                        temp_min = (double)rascunho["main"]["temp_min"],
+
+                        temp = (double)rascunho["main"]["temp"],
+                        feels_like = (double)rascunho["main"]["feels_like"],
+                        visibility = (int)rascunho["visibility"],
+                        sunrise = sunrise,
+                        sunset = sunset,
+                        timezone = (int)rascunho["timezone"],
+                        icon = (string)rascunho["weather"][0]["icon"]
+                    };
+                }
+            }
+            return t;
+        }
+    }
+}
